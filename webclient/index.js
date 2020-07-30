5c242eb8ed91817ea9deb4dd5a0041795a7349fa
yeeBones <doddsbt@gmail.com>
2020-07-30 01:37:57 +0100

    Update index.js

--- webclient/index.js
+++ webclient/index.js
@@ -4,15 +4,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Enter IP adress : ", function (IP) {     //this is hardcoded now to 192.168.0.11
-    rl.question("Enter port : ", function (port) {    //this is hardcoded for now to 6969
+rl.question("Enter IP adress : ", function (IP) {     
+    //rl.question("Enter port : ", function (port) {    //this is hardcoded for now to 6969 on the server if you wanna change it feel free
         rl.question("Enter data to send : ", function (word) {
             var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
             var xmlhttp = new XMLHttpRequest()
-            xmlhttp.open("POST", "http://" + IP + ":" + port);
+            xmlhttp.open("POST", "http://" + IP + ":" + 6969);
             xmlhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
             xmlhttp.send(word);
             rl.close();
         });
-    });
+    //});
 });
